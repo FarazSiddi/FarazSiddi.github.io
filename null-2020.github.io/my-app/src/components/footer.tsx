822a714ddbf485870e9535b74b0bbe48c9f5b430
null-2020 <f1.siddiqui@yandex.com>
2022-08-19 22:42:34 -0400

    Added Navigation Bar

---
+++ null-2020.github.io/my-app/src/components/footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <header>
+      <h1>This is a footer</h1>
+    </header>
+  );
+};
---
+++ null-2020.github.io/my-app/src/components/main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Main = () => {
+  return (
+    <header>
+      <h1>This is the main (body)</h1>
+    </header>
+  );
+};
