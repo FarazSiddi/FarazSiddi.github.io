2477edea4580fd7a98b00e55d72da6fb6c7e8e3b
null-2020 <f1.siddiqui@yandex.com>
2022-09-27 14:42:56 -0400

    Made the first banner more responsive when a user shrinks or grows the window page

--- null-2020.github.io/my-app/src/components/about.tsx
+++ null-2020.github.io/my-app/src/components/about.tsx
@@ -4,14 +4,16 @@ import "./styles/about.css";
 export const About = () => {
   return (
     <div id="about-me" className="bg-light">
-      <div className="d-flex justify-content-between ps-5 pt-5">
-        <img
-          id="headshoot"
-          src={require("./images/Headshot-FarazSiddiqui_3.jpg")}
-          className="border border-info border-5 rounded-circle"
-          alt="A headshot of myself wearing a gray jacket and black collared shirt."
-        />
-        <h1>Hi!</h1>
+      <div className="container ps-5 pt-5">
+        <div className="row">
+          <img
+            id="headshoot"
+            src={require("./images/Headshot-FarazSiddiqui_3.jpg")}
+            className="col-md-5 border border-info border-5 rounded-circle me-5"
+            alt="A headshot of myself wearing a gray jacket and black collared shirt."
+          />
+          <h1 className="col-md-8">Hi!</h1>
+        </div>
         <p>
           I am currently a second year Applied Mathematics student with a
           specialization of Computer Science at Toronto Metropolitan University
--- null-2020.github.io/my-app/src/components/banner.tsx
+++ null-2020.github.io/my-app/src/components/banner.tsx
@@ -3,16 +3,8 @@ import "./styles/banner.css";
 
 export const Banner = () => {
   return (
-    <div id="banner" className="bg-success w-100 position-relative">
-      <img
-        id="background-img"
-        alt="Snowy mountains in a beautiful night sky"
-        src={require("./images/teahub.io-snow-mountain-wallpaper-572052.jpg")}
-      />
-      <div
-        id="banner--center"
-        className="position-absolute top-50 start-50 translate-middle"
-      >
+    <div id="banner" className="position-relative">
+      <div id="banner--center">
         <div id="banner--text" className="text-center px-5 py-5">
           <h1 id="text--top">Faraz Siddiqui</h1>
           <h3 id="text--bottom">Aspiring Full-Stack Developer</h3>
