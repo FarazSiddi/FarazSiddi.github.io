01eebaa34653df5fb655da675cb8bb1c91771d39
null-2020 <f1.siddiqui@yandex.com>
2022-10-17 12:58:47 -0400

    Fixed 'About Me' Section and added buttons in 'Skills' section

--- null-2020.github.io/my-app/src/components/about.tsx
+++ null-2020.github.io/my-app/src/components/about.tsx
@@ -4,7 +4,7 @@ import "./styles/about.css";
 export const About = () => {
   return (
     <div id="about-me" className="bg-light">
-      <div className="container ps-5 pt-5">
+      <div className="container ps-5 pt-5 align-middle">
         <div className="d-flex flex-row bd-highlight mb-3">
           <img
             id="headshoot"
@@ -18,9 +18,18 @@ export const About = () => {
               My name is Faraz Siddiqui. I am currently a second year Applied
               Mathematics student with a specialization of Computer Science at
               Toronto Metropolitan University (formerly known as Ryerson
-              University). I love probem solving.
+              University). I love to solve many problems, whether it has to do
+              with coding, mathematics, algorithms, and more. I love to code in
+              Python, Java, and JavaScript/TypeScript, as well has having plans
+              to get familiar with C++ and Rust in the near future. Furthermore,
+              I am open to learn new things as well as working on interesting
+              projects. Feel free to reach out to me via e-mail or LinkedIn.
             </p>
-            <p className="bd-highlight">Website and resume:</p>
+            <p className="bd-highlight">Portfolio: null-2020.github.io</p>
+            <p className="bd-highlight">GitHub: github.com/null-2020</p>
+            <p className="bd-highlight">
+              Linkedin: linkedin.com/faraz-siddiqui-r
+            </p>
           </div>
         </div>
       </div>
--- null-2020.github.io/my-app/src/components/skills.tsx
+++ null-2020.github.io/my-app/src/components/skills.tsx
@@ -3,8 +3,21 @@ import "./styles/skills.css";
 
 export const Skills = () => {
   return (
-    <div id="skills" className="bg-dark">
-      <h3 className="text-light">List of skills</h3>
+    <div id="skills">
+      <h3>Skills:</h3>
+      <h4>Programming Languages:</h4>
+      <div className="container">
+        <div className="row">
+          <button className="col buttons">Python</button>
+          <button className="col buttons">JavaScript and TypeScript</button>
+          <button className="col buttons">Java</button>
+        </div>
+        <div className="row">
+          <button className="col buttons">Bash</button>
+          <button className="col buttons">C++</button>
+          <button className="col buttons">Rust</button>
+        </div>
+      </div>
     </div>
   );
 };
